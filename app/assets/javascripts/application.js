1828e8b7bff685062871a3a367157ef9e4ca0038
myzen2 <mehdi.zenbil@orange.fr>
2016-01-29 13:38:49 +0100

    adding css and javascript to navbar fixed after scroll

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,19 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap-sprockets
+
+$(document).ready(function() {
+
+$(window).scroll(function () {
+    //if you hard code, then use console
+    //.log to determine when you want the
+    //nav bar to stick.
+    console.log($(window).scrollTop())
+  if ($(window).scrollTop() > 280) {
+    $('#nav_bar').addClass('navbar-fixed');
+  }
+  if ($(window).scrollTop() < 281) {
+    $('#nav_bar').removeClass('navbar-fixed');
+  }
+});
+});
